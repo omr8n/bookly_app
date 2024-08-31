7c4ada2772ade8027bb23d1e604bc8b8a1cd70c7
Your Name <omranaboaliii@gmail.com>
2024-08-31 17:47:38 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:bookly_app/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'Features/Splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
+      home: const SplashView(),
+    );
+  }
+}
